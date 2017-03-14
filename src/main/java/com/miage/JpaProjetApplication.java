0a4c9222d636694a55b4ef606b3b65c0d846561c
doclapin <sim_conejo@yahoo.fr>
2017-03-14 12:14:54 +0100

    commit test

---
+++ src/main/java/com/miage/JpaProjetApplication.java
@@ -0,0 +1,12 @@
+package com.miage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaProjetApplication.class, args);
+	}
+}
