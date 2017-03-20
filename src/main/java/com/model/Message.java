9d15e88e1d6f7070240c1b9368552a9bbe0f9313
DamDam <dam.jacquet@gmail.com>
2017-03-20 16:06:43 +0100

    Creation des classes + getter / setter

---
+++ src/main/java/com/model/Message.java
@@ -0,0 +1,14 @@
+package com.model;
+
+import java.util.Date;
+
+public class Message {
+	
+	private long idMessage;
+	private long idExp;
+	private long idDest;
+	private String message;
+	private Date date;
+	
+
+}
